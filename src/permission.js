89afff77efb9b2585b62b1ab260e67fa8e065728
jetlee <itlijinpeng@outlook.com>
2020-04-24 00:07:10 +0800

    init

---
+++ src/permission.js
@@ -0,0 +1,74 @@
+import Vue from 'vue'
+import router from './router'
+import store from './store'
+
+import NProgress from 'nprogress' // 引入页面加载进度条
+import '@/components/NProgress/nprogress.less' // 页面加载进度条样式
+import notification from 'ant-design-vue/es/notification'
+import { setDocumentTitle, domTitle } from '@/utils/domUtil'
+import { ACCESS_TOKEN } from '@/store/mutation-types'
+
+NProgress.configure({ showSpinner: false }) // 进度条配置
+
+const whiteList = ['login', 'register', 'registerResult'] // no redirect whitelist
+const defaultRoutePath = '/dashboard/workplace'
+
+router.beforeEach((to, from, next) => {
+  NProgress.start() // 开始页面加载进度条
+  to.meta && (typeof to.meta.title !== 'undefined' && setDocumentTitle(`${to.meta.title} - ${domTitle}`))
+  if (Vue.ls.get(ACCESS_TOKEN)) {
+    // 当token存在时,如果路径为登录页那么跳转到默认页
+    if (to.path === '/user/login') {
+      next({ path: defaultRoutePath })
+      NProgress.done()
+    } else {
+      console.log(11111111)
+      console.log(store.getters.roles)
+      if (store.getters.roles.length === 0) {
+        store
+          .dispatch('GetInfo')
+          .then(res => {
+            const roles = res.result && res.result.role
+            store.dispatch('GenerateRoutes', { roles }).then(() => {
+              // 根据roles权限生成可访问的路由表
+              // 动态添加可访问路由表
+              router.addRoutes(store.getters.addRouters)
+              // 请求带有 redirect 重定向时，登录自动重定向到该地址
+              const redirect = decodeURIComponent(from.query.redirect || to.path)
+              if (to.path === redirect) {
+                // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+                next({ ...to, replace: true })
+              } else {
+                // 跳转到目的路由
+                next({ path: redirect })
+              }
+            })
+          })
+          .catch(() => {
+            notification.error({
+              message: '错误',
+              description: '请求用户信息失败，请重试'
+            })
+            store.dispatch('Logout').then(() => {
+              next({ path: '/user/login', query: { redirect: to.fullPath } })
+            })
+          })
+      } else {
+        next()
+      }
+    }
+  } else {
+    if (whiteList.includes(to.name)) {
+      // 判断,如果Toekn不存在,在免登录白名单，直接进入
+      next()
+    } else {
+      // 不在免登录白名单内,直接跳转到登录页面
+      next({ path: '/user/login', query: { redirect: to.fullPath } })
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  NProgress.done() // finish progress bar
+})
